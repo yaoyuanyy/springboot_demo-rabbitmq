5773f2abaa2801d7f414e9007f3c760f00c643e8
yaoliang(姚亮.企业云事业部.基础研发部) <yaoliang@ds.gome.com.cn>
2017-07-02 14:51:35 +0800

    amend dependency jar

--- src/main/java/com/yy/service/IReceiverService.java
+++ src/main/java/com/yy/service/IReceiverService.java
@@ -1,6 +1,7 @@
 package com.yy.service;
 
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,12 +10,13 @@ import org.springframework.stereotype.Service;
  * Created by yaoliang on 2017/2/24.
  */
 @Service
+@RabbitListener(queues = "springboot_rmq")
 public class IReceiverService {
 
     @Autowired
     private AmqpTemplate rabbitTemplate;
 
-    @RabbitListener(queues = "springboot_rmq")
+    @RabbitHandler
     public void receiveMessage(String message) {
         System.out.println("Received <" + message + ">");
     }
